336b21ca4de4eceade4cf52fff71aa93d1198853
Matthew Patrick <mattimpat@gmail.com>
2018-06-10 15:40:16 -0400

    Update runUnconditional.sh

--- ConditionalAnalysis/runUnconditional.sh
+++ ConditionalAnalysis/runUnconditional.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-FOLD=$1
 function runAssociation(){
 COHORT=$1; TEST=$2; STEP=$3
-plink2 --pfile ../data/${COHORT}_${TEST}${FOLD} --pheno ../data/${COHORT}_${TEST}_${FOLD}.phe --pheno-name ${TEST} --covar ../data/${COHORT}_${TEST}_${FOLD}.covar --glm hide-covar cols=+beta --threads 10 --out ../results/${STEP}/plink2.${COHORT}${FOLD}
-awk 'BEGIN{print "MARKER\tBeta\tSE"} NR>1{print $3"\t"$8"\t"$9}' ../results/${STEP}/plink2.${COHORT}${FOLD}.${TEST}.glm.logistic | sort -nk1,1 > ../results/${STEP}/${COHORT}.${TEST}${FOLD}.dat
+plink2 --pfile ../data/${COHORT}_${TEST} --pheno ../data/${COHORT}_${TEST}.phe --pheno-name ${TEST} --covar ../data/${COHORT}_${TEST}.covar --glm hide-covar cols=+beta --threads 10 --out ../results/${STEP}/plink2.${COHORT}
+awk 'BEGIN{print "MARKER\tBeta\tSE"} NR>1{print $3"\t"$8"\t"$9}' ../results/${STEP}/plink2.${COHORT}.${TEST}.glm.logistic | sort -nk1,1 > ../results/${STEP}/${COHORT}.${TEST}.dat
 }
 
 STEP=0
@@ -12,6 +11,6 @@ for COHORT in CASP Exomechip Genizon KielGWAS PsAGWAS; do
 runAssociation ${COHORT} psa_v_ctl ${STEP}; done
 for COHORT in CASP Exomechip Genizon KielGWAS; do
 runAssociation ${COHORT} psc_v_ctl ${STEP}; done
-./runMETAL.sh ${STEP} ${FOLD}
-BESTMARKER=$(cat ../results/${STEP}/PsAPsC${FOLD}.indirect | awk 'BEGIN{getline;print $1}')
-echo ${BESTMARKER} > ../data/condition${FOLD}.dat
+./runMETAL.sh ${STEP}
+BESTMARKER=$(cat ../results/${STEP}/PsAPsC.indirect | awk 'BEGIN{getline;print $1}')
+echo ${BESTMARKER} > ../data/condition.dat
