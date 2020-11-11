7a2ff4aa7f669af5b55eae36bae71b535675e280
Nidhi Jaison <nidhijaison@gmail.com>
2020-11-11 08:34:56 -0500

    draft of compareLoci

---
+++ LocusIdentification/compareLoci.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+def compareLoci(path1, path2):
+    df1 = pd.read_csv(path1)
+    df2 = pd.read_csv(path2)
+
+    loci1 = df1.to_numpy()
+    loci2 = df2.to_numpy()
+
+    similarloci = []
+    for i in range(0,len(loci1)):
+        for j in range(0,len(loci2)):
+            if abs(loci1[i][0] - loci2[j][0]) >= 500:
+                similarloci.append(loci2)
+
+    return similarloci
