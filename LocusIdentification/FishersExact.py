00d6a27f15d34db4d7693f8def20a5453fdb7b17
Nidhi Jaison <nidhijaison@gmail.com>
2021-02-25 00:23:56 -0500

    update FishersExact.py

--- LocusIdentification/FishersExact.py
+++ LocusIdentification/FishersExact.py
@@ -1,28 +1,38 @@
 from scipy import stats
 import numpy as np
+import pandas as pd
 
 
 def fishersExactTest(binaryFile, alternative):
-    firstBEDnumones = 0
-    firstBEDnumzeros = 0
-    BEDnumones = 0
-    BEDnumzeros = 0
+    numFirstFileOnes = 0
+    numFirstFileZeros = 0
+    numBothOnes = 0
+    numBothZeros = 0
+    p_values = []
+    oddsratios = []
+    filenames = []
     with open(binaryFile) as f:
         for line in f:
             L = line.strip().split()
-            if L[2] == 0:
-                firstBEDnumones = firstBEDnumones + 1
-            elif L[2] == 1:
-                firstBEDnumzeros = firstBEDnumzeros + 1
-
             for i in range(3, len(L)):
-                if L[i] == 0:
-                    BEDnumones = BEDnumones + 1
-                elif L[i] == 1:
-                    BEDnumzeros = BEDnumzeros + 1
+                if L[i] == 0 and L[2] == 0:
+                    numBothZeros = numBothZeros + 1
+                elif L[i] == 1 and L[2] == 1:
+                    numBothOnes = numBothOnes + 1
+                elif L[i] == 0 and L[2] == 1:
+                    numFirstFileOnes = numFirstFileOnes + 1
+                elif L[i] == 1 and L[2] == 0:
+                    numFirstFileZeros = numFirstFileZeros + 1
+                elif L[0] == "chr":
+                    filenames.append(L[i])
 
-    contingency = np.array([[firstBEDnumones, BEDnumones], [firstBEDnumzeros, BEDnumzeros]])
+            contingency = np.array([[numBothOnes, numFirstFileOnes], [numFirstFileZeros, numBothZeros]])
+            p_value, oddsratio = stats.fisher_exact(contingency, alternative)
 
-    p_value, oddsratio = stats.fisher_exact(contingency, alternative)
+            p_values.append(p_value)
+            oddsratios.append(oddsratio)
 
-    return p_value
+    # return a dataframe with filename and p value
+    finalDataset = np.vstack((p_values, oddsratios))
+    final_df = pd.DataFrame(data=finalDataset, columns=filenames)
+    return final_df
