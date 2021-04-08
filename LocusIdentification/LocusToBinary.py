3aa17085f68484c2f85682ff3ba61f2c5bc144c5
Nidhi Jaison <nidhijaison@gmail.com>
2021-04-08 08:55:45 -0400

    updates

--- LocusIdentification/LocusToBinary.py
+++ LocusIdentification/LocusToBinary.py
@@ -3,63 +3,47 @@ import numpy as np
 
 # filenames is a list of the BED documents to compare
 # lociFile is the list of loci
-def convert(lociFile, markersFile, BEDfilenames):
+def convert(markersFile, BEDfilenames):
     # initial variables
     chromosomes = []
     lociPositions = []
-    markersData = []  # 2-D array of data from markers file, with each element in format: (chr, pos, lociPos)
+    positions = []
     binaryData = []
     headers = ["chr", "pos"]
-
-    # Read through locus file, logging each position
-    with open(lociFile) as f:
-        for line in f:
-            L = line.strip().split()
-            chromosomes.append(L[0])
-            lociPositions.append(L[1])
 
     with open(markersFile) as f:
         for line in f:
             L = line.strip().split()
-            markersData.append((L[0], L[1], L[3]))  # (chr, pos, lociPos)
+            chromosomes.append(L[0])
+            lociPositions.append(L[3])
+            positions.append(L[1])  # (chr, pos, lociPos)
+
+    loci = np.unique(lociPositions).sort()
 
     # Read through each remaining BED file
     for file in BEDfilenames:
         with open(file) as f:
-
             # Add filename to header.
             headers.append(file)
-            for i in range(0, len(lociPositions)):
-                found = False
-                for line in f:
-                    L = line.strip().split()
-                    binary = []
-                    # If the chromosomes being currently compared are the same, continue
-                    if int(L[0]) == chromosomes[i]:
-                        # create a new list of "relevant markers", which are markers that have the current chromosome
-                        relevantMarkers = markersData[:, markersData[0] != chromosomes[i]]
-                        # then filter these so the relevant markers are only those of the current locus.
-                        relevantMarkers = relevantMarkers[:, relevantMarkers[3] != lociPositions[i]]
-                        # If the position of the locus itself overlaps with a BED position, then append "1".
-                        if isOverlapping(lociPositions[i], [L[1], L[2]]):
-                            binary.append(1)
-                            found = True
-                            break
-                        else:
-                            # Otherwise, loop through each marker that belongs to this locus,
-                            # and see if its position overlaps
-                            for j in range(0, len(relevantMarkers)):
-                                if isOverlapping(relevantMarkers[j], [L[1], L[2]]):
-                                    binary.append(1)
-                                    found = True
-                                    break
+            for j in range(loci):
+                for i in range(0, len(positions)):
+                    found = False
+                    for line in f:
+                        L = line.strip().split()
+                        binary = []
+                        # If the chromosomes being currently compared are the same, continue
+                        if int(L[0]) == chromosomes[i] and loci[j] == lociPositions[i]:
+                            if isOverlapping(lociPositions[i], [L[1], L[2]]) or isOverlapping(positions[i], [L[1], L[2]]):
+                                binary.append(1)
+                                found = True
+                                break;
                 if not found:
                     binary.append(0)
 
             # Add binary data from this BED file to the "master" matrix.
             binaryData.append(binary)
 
-    finalMatrix = np.vstack((chromosomes, lociPositions, binaryData))
+    finalMatrix = np.vstack((chromosomes, loci, binaryData))
     np.transpose(finalMatrix)
     finalMatrix = np.vstack((headers, finalMatrix))
     return finalMatrix
--- LocusIdentification/linkageDisequilibrium.py
+++ LocusIdentification/linkageDisequilibrium.py
@@ -6,18 +6,21 @@ import numpy as np
 def linkageDisequilibrium(lociPath, LDPath, LDthreshold, outputfile):
     locidf = pd.read_csv(lociPath)
     LDdf = pd.read_csv(LDPath)
-    lociPositions = np.array(locidf["pos"].to_list())
+    markerPositions = np.array(locidf["pos"].to_list())
+    markerChr = np.array(locidf["chr"].to_list())
+    markerPVal = np.array(locidf["p_value"].to_list())
+    lociPos = np.array(locidf["correspondingLociPos"].to_list())
     LD = LDdf.to_numpy()
 
     markers = []
 
-    # Please present the results with the locus marker first, the LD (surrogate) marker second, and the LD value third.
-
     for i in range(0, len(LD)):
-        if (LD[i][2]) >= LDthreshold and (LD[i][0] in lociPositions or LD[i][1] in lociPositions):
-            markers.append([lociPositions[i], LD[i][0], LD[i][2]])
+        if (LD[i][2]) >= LDthreshold:
+            for j in range(0, len(markerPositions)):
+                if LD[i][0] == j or LD[i][1] == j:
+                    markers.append([markerPositions[j], markerChr[j], markerPVal[j], lociPos[j], LD[i][0], LD[i][2]])
 
-    markers_df = pd.DataFrame(data=markers, columns=["locusMarker", "surrogateMarker", "LDval"])
+    markers_df = pd.DataFrame(data=markers, columns=["pos", "chr", "p_value", "correspondingLociPos", "surrogateMarker", "LDval"])
     markers_df.to_csv(outputfile) if outputfile != " " else print(markers_df)
     sys.stderr.write("Markers file created.\n")
     return markers
